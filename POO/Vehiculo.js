8723d745d25fb4f1bdd801ee7085757e686d6184
l-cast07 <23050009@itsn.edu.mx>
2026-09-15 14:17:23 -0600

    Actualización de archivos importantes

--- POO/Vehiculo.js
+++ POO/Vehiculo.js
@@ -1,35 +1,54 @@
-class Vehiculo{
-    constructor(marca, modelo){
+class Vehiculo {
+    constructor(marca, modelo) {
         this.marca = marca;
         this.modelo = modelo;
     }
 
-    mostrarInfo(){
-        return `Este coche es marca ${this.marca} es un ${this.modelo}`;
+    mostrarInfo() {
+        console.log("La marca es " + this.marca + " y el modelo es " + this.modelo);
+    }
+
+    encender() {
+        console.log("El vehículo está encendido.");
     }
 }
 
-class Coche extends Vehiculo{
-    constructor(marca, puertas) {
-        super(marca, 'Algun Modelo');
+class Coche extends Vehiculo {
+    constructor(marca, modelo, puertas) {
+        super(marca, modelo);
         this.puertas = puertas;
     }
 
-    mostrarPuertas(){
-        return `El carro ${this.marca} tiene ${this.puertas} puertas`;
+    mostrarPuertas() {
+        console.log("Este coche tiene " + this.puertas + " puertas.");
     }
 }
 
-class Moto extends Vehiculo{
-    constructor(marca, llantas) {
-        super(marca, '2');
-        this.llantllantas =llantas;
+class Moto extends Vehiculo {
+    constructor(marca, modelo, cilindrada) {
+        super(marca, modelo);
+        this.cilindrada = cilindrada;
     }
 
-    mostrarLLantas(){
-        return `La moto ${this.marca} tiene ${this.llantllantas} llantas`;
+    mostrarCilindrada() {
+        console.log("La moto tiene una cilindrada de " + this.cilindrada);
     }
 }
 
-const Carro = new Coche('El del guero','4');
-console.log(Carro.mostrarPuertas());
+const miVehiculo = new Vehiculo("Nissan", "Tsuru");
+miVehiculo.mostrarInfo();
+miVehiculo.encender();
+
+console.log("-------------------");
+
+const miCoche = new Coche("Chevrolet", "Aveo", 4);
+miCoche.mostrarInfo();
+miCoche.mostrarPuertas();
+miCoche.encender();
+
+console.log("-------------------");
+
+const miMoto = new Moto("Italika", "DM200", "200cc");
+miMoto.mostrarInfo();
+miMoto.mostrarCilindrada();
+miMoto.encender();
